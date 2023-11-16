0da94e9750851838d4b0893802822e06982f8a49
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2023-11-16 12:56:31 +0000

    Lista Squadre

--- NBA/src/app/app.module.ts
+++ NBA/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PlayersListComponent } from './players-list/players-list.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    PlayersListComponent,
+    PlayerDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- NBA/src/app/homepage/homepage.component.ts
+++ NBA/src/app/homepage/homepage.component.ts
@@ -9,5 +9,19 @@ import { Squadre } from '../models/teams.model';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  title= 'NBAProject'
+  obs!:Observable<Squadre>
+  data!:Squadre
 
+ // Richiesta al server e assegnazione dei dati
+  constructor(public http: HttpClient) {
+    this.obs = this.http.get<Squadre>("https://www.balldontlie.io/api/v1/teams")
+    this.obs.subscribe(this.dosomething)
+
+  }
+
+  dosomething = (data: Squadre) => {
+    this.data = data;
+    console.log(this.data)
+  }
 }
